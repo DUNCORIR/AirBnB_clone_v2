3c068356d6112ac12d75166f358db94044ca5f00
duncorir <duncorir@mail.com>
2025-01-02 16:37:15 +0300

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,5 +7,8 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grant all privileges on hbnb_test_db to the user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
+-- Grant SELECT privilege on performance_schema to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
 -- Apply changes
 FLUSH PRIVILEGES;
