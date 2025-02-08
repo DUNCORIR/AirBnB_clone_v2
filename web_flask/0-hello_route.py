cf58de6cf469031c8e059883368d74b3153178c7
duncorir <duncorir@mail.com>
2025-02-08 18:16:28 +0300

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Returns a simple greeting message"""
-    return "Hello HBNB!\n"
+    return "Hello HBNB!$\n"
 
 
 if __name__ == "__main__":
