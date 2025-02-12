f5ff8f66fcf2d37a42c2af7da06a24004cf8c4f9
duncorir <duncorir@mail.com>
2025-02-12 16:00:36 +0300

    hbnb live

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -7,17 +7,22 @@ from models.amenity import Amenity
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Remove the current SQLAlchemy Session"""
     storage.close()
+
 
 @app.route('/hbnb_filters', strict_slashes=False)
 def hbnb_filters():
     """Displays a HTML page with filters for states, cities, and amenities"""
     states = sorted(storage.all(State).values(), key=lambda s: s.name)
     amenities = sorted(storage.all(Amenity).values(), key=lambda a: a.name)
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+    return render_template(
+        '10-hbnb_filters.html', states=states, amenities=amenities
+    )
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
